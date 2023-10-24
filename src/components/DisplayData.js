7bfe49e5fcecbbeb77852c06b6a3d86bcaa9117b
saif khan <saifbusiness29@gmail.com>
2023-10-24 15:29:30 +0530

    bg colors added

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -1,102 +1,102 @@
-import React, { useEffect } from 'react'
-import sunny from '../assets/sunny.jpg'
-import hazy from '../assets/hazy.jpg'
-import rainy from '../assets/rainy.jpg'
-import cloud from '../assets/cloudy.jpg'
-import cold from '../assets/cold.jpg'
+import React, { useEffect, useState } from "react";
+import sunny from "../assets/sunny.jpg";
+import hazy from "../assets/hazy.jpg";
+import rainy from "../assets/rainy.jpg";
+import cloud from "../assets/cloudy.jpg";
+import cold from "../assets/cold.jpg";
 // import calm from '../assets/calm.jpg'
 
 const DisplayData = (props) => {
+  const [loading, setLoading] = useState(false);
 
-    console.log('inside of display data')
-    console.log(props.weatherData)
+  const weather_day = [
+    {
+      icon: hazy,
+      text: "hazy",
+      color: '#CCCCCC'
+    },
+    {
+      icon: rainy,
+      text: "Rain",
+      color: '#0366d6'
+    },
+    {
+      icon: cloud,
+      text: "Clouds",
+      color: '#cccccc'
+    },
+    {
+      icon: cold,
+      text: "Cold",
+      color: '#2541B2'
+    },
+    {
+      icon: sunny,
+      text: "Clear",
+      color: '#FFD700'
+    },
+  ];
 
-    const weather_day = [
-        {
-            icon: hazy,
-            text: 'hazy'
-        },
-        {
-            icon: rainy,
-            text: 'Rain'
-        },
-        {
-            icon: cloud,
-            text: 'Clouds'
-        },
-        {
-            icon: cold,
-            text: 'Cold'
-        },
-        {
-            icon: sunny,
-            text: 'Clear'
+  const { name, main, weather } = props.weatherData;
+  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
+
+  useEffect(() => {
+    setLoading(true);
+      const bodyElement = document.querySelector("body");
+      weather_day.forEach((item, idx) => {
+        if (item.text === weather[0].main) {
+        //   bodyElement.style.backgroundImage = `url(${item.icon})`;
+          bodyElement.style.backgroundColor = item.color
         }
-    ]
+        setLoading(false);
+      });
 
-
-    const { name, main, weather } = props.weatherData
-    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
-
-    useEffect(() => {
-        const bodyElement = document.querySelector('body');
-
-        weather_day.forEach((item, idx) => {
-            if (item.text === weather[0].main) {
-                bodyElement.style.backgroundImage = `url(${item.icon})`
-                console.log(item.text)
-                console.log(weather[0].main)
-            }
-        })
-
-
-    }, [weather]);
+    
+  }, [weather]);
 
 
 
-
-    return (
-        <div>
-            <div className="display_data_cont d-flex flex-column">
-                <div className='main_info '>
-
-                    <div className="inner_cont d-flex justify-content-center align-items-center">
-                        <h2 className='me-3' >{name}</h2>
-                        <h3>{main.temp} °C</h3>
-                    </div>
-                    <div className="weather_info_cont">
-                        <div className="inner_cont d-flex justify-content-start align-items-center">
-                            <h3 className='me-3' >{weather[0].main}</h3>
-                            <img src={iconUrl} alt="" />
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="data-weather_info_cont d-flex justify-content-between pt-4 md-0 py-5-xl my-5-xl">
-                    <div className="inner_cont items">
-                        <h2>Feels Like</h2>
-                        <h3>{main.feels_like}</h3>
-                    </div>
-                    <div className="inner_cont items">
-                        <h2>Minimum Temperature</h2>
-                        <h3>{main.temp_min}</h3>
-                    </div>
-                    <div className="inner_cont items">
-                        <h2>Maximum Temperature</h2>
-                        <h3>{main.temp_max}</h3>
-                    </div>
-                    <div className="inner_cont items">
-                        <h2>Humidity</h2>
-                        <h3>{main.humidity}</h3>
-                    </div>
-
-                </div>
-
+  return (
+    <div>
+      {loading ? (
+        <h2>Waiting for results...</h2>
+      ) : (
+        <div className="display_data_cont d-flex flex-column">
+          <div className="main_info ">
+            <div className="inner_cont d-flex justify-content-center align-items-center">
+              <h2 className="me-3">{name}</h2>
+              <h3>{main.temp} °C</h3>
             </div>
+            <div className="weather_info_cont">
+              <div className="inner_cont d-flex justify-content-start align-items-center">
+                <h3 className="me-3">{weather[0].main}</h3>
+                <img src={iconUrl} alt="" />
+              </div>
+            </div>
+          </div>
 
+          <div className="data-weather_info_cont d-flex justify-content-between pt-4 md-0 py-5-xl my-5-xl">
+            <div className="inner_cont items">
+              <h2>Feels Like</h2>
+              <h3>{main.feels_like}</h3>
+            </div>
+            <div className="inner_cont items">
+              <h2>Minimum Temperature</h2>
+              <h3>{main.temp_min}</h3>
+            </div>
+            <div className="inner_cont items">
+              <h2>Maximum Temperature</h2>
+              <h3>{main.temp_max}</h3>
+            </div>
+            <div className="inner_cont items">
+              <h2>Humidity</h2>
+              <h3>{main.humidity}</h3>
+            </div>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default DisplayData
+export default DisplayData;
--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,81 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import DisplayData from './DisplayData'
-import './Weather.scss'
-
+import React, { useState, useEffect } from "react";
+import DisplayData from "./DisplayData";
+import "./Weather.scss";
 
 const GetData = () => {
-    const [currentCity, setCurrentCity] = useState('')
-    const [city, setCity] = useState('')
-    const [weatherData, setWeatherData] = useState(null);
+  const [currentCity, setCurrentCity] = useState("");
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
+  function inputHandler(e) {
+    setCurrentCity(e.target.value);
+  }
 
+  let API_key = "050637cb59abe2b93968f907ef97451d";
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_key}&units=metric`
+      );
 
-    function inputHandler(e) {
-        setCurrentCity(e.target.value)
+      if (!response.ok) {
+        setCity(currentCity);
+        throw new Error("weather data not found");
+      }
+
+      const data = await response.json();
+      setWeatherData(data);
+      console.log(weatherData);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setWeatherData(null);
     }
 
-    let API_key = '050637cb59abe2b93968f907ef97451d'
+    setCurrentCity("");
+  };
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-
-
-
-
-        try {
-            const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_key}&units=metric`
-
-            )
-
-            if (!response.ok) {
-                setCity(currentCity)
-                throw new Error('weather data not found')
-            }
-
-            const data = await response.json()
-            setWeatherData(data)
-            console.log(weatherData)
-        } catch (error) {
-            console.error('Error fetching weather data:', error);
-            setWeatherData(null);
-        }
-
-
-        setCurrentCity('');
-
-
-
-
-    }
-
-    const Error_message = () => {
-        return (
-            <div>
-                <h1>Please enter a valid city name.</h1>
-            </div>
-        )
-    }
-
-    useEffect(() => {
-        console.log(weatherData);
-    }, [weatherData]);
-
+  const Error_message = () => {
     return (
-        <div className='get_data_main_cont d-flex flex-column' >
-            <form className='get_data_cont my-4' onSubmit={submitHandler}>
-                <input type="text" value={currentCity} name="" id="" onChange={inputHandler} />
-                <button type='submit'>Submit</button>
-            </form>
-            {weatherData != null && <DisplayData weatherData={weatherData} city={city} />}
-            {weatherData == null && city != '' && <Error_message />}
+      <div>
+        <h1>Please enter a valid city name.</h1>
+      </div>
+    );
+  };
 
 
 
-        </div>
-    )
-}
+  return (
+    <div className="get_data_main_cont d-flex flex-column">
+      <form className="get_data_cont my-4" onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={currentCity}
+          name=""
+          id=""
+          onChange={inputHandler}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {weatherData != null && (
+        <DisplayData weatherData={weatherData} city={city} />
+      )}
+      {weatherData == null && city != "" && <Error_message />}
+    </div>
+  );
+};
 
-export default GetData
+export default GetData;
